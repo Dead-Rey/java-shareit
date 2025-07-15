dfc1e2a152ac6cac4b6c832d35ae8fc42d782a06
Dmitry Zhukov <juckow.d@yandex.ru>
2025-07-15 15:24:47 +0500

    Фикс ошибок

--- src/main/java/ru/practicum/shareit/controllers/BookingController.java
+++ src/main/java/ru/practicum/shareit/controllers/BookingController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.controllers;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,31 +21,31 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping
-    public BookingDto createBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public BookingDto createBooking(@RequestHeader ("X-Sharer-User-Id") @Positive Long userId,
                                     @Valid @RequestBody BookingCreateDto booking) {
         return bookingService.createBooking(booking, userId);
     }
 
     @PatchMapping(path = "/{bookingId}")
-    public BookingDto approveBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public BookingDto approveBooking(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
                                      @PathVariable Long bookingId,
                                      @RequestParam boolean approved) {
         return bookingService.approveBooking(bookingId, userId, approved);
     }
 
     @GetMapping(path = "/{bookingId}")
-    public BookingDto getBooking(@PathVariable Long bookingId) {
+    public BookingDto getBooking(@PathVariable @Positive Long bookingId) {
         return bookingService.getBooking(bookingId);
     }
 
     @GetMapping
-    public List<BookingDto> getBookingsByBookerId(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public List<BookingDto> getBookingsByBookerId(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
                                                   @RequestParam(defaultValue = "ALL") TimeStatus status) {
         return bookingService.getBookingByBookerIdAndStatus(userId, status);
     }
 
     @GetMapping(path = "/owner")
-    public List<BookingDto> getBookingsByItemOwnerId(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public List<BookingDto> getBookingsByItemOwnerId(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
                                                      @RequestParam(defaultValue = "ALL") TimeStatus status) {
         return bookingService.getBookingByItemIdAndStatus(userId, status);
     }
--- src/main/java/ru/practicum/shareit/exeptions/GlobalExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/exeptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.exeptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,6 +17,7 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -26,8 +28,10 @@ public class GlobalExceptionHandler {
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
 
+        log.warn("Ошибка  валидации: {}", errors);
+
         Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("errors", errors);
+        responseBody.put("Ошибки:", errors);
 
         return ResponseEntity.badRequest()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -36,37 +40,45 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFound(NoSuchElementException ex) {
+        log.warn("Ресурс не найден: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResponse> handleIllegalState(IllegalStateException ex) {
+        log.warn("Некорректное состояние: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException ex) {
+        log.warn("Не найдено: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleAlreadyExists(AlreadyExistsException ex) {
+        log.warn("Уже существует: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        log.warn("Нечитаемый запрос: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDenyException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDenyException(AccessDenyException ex) {
+        log.warn("Отказ в доступе: {}", ex.getMessage());  // Логируем отказ в доступе
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getMessage()));
-    }
-
-    @ExceptionHandler(AccessDenyException.class)
-    public ResponseEntity<ErrorResponse> handleAccessDenyException(AccessDenyException ex) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(ex.getMessage()));
+        log.error("Непредвиденная ошибка сервера: ", ex);  // Логируем полный stacktrace для неожиданных ошибок
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("Непредвиденная ошибка сервера: " + ex.getMessage()));
     }
 }
